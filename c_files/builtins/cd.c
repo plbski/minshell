e77197159f642e1f9a42d3815746206a6950b782
Giulio Valente <gvalente@c2r6s1.42lausanne.ch>
2025-02-10 18:49:07 +0100

    done

--- c_files/builtins/cd.c
+++ c_files/builtins/cd.c
@@ -6,7 +6,7 @@
 /*   By: gvalente <gvalente@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/14 22:29:41 by gvalente          #+#    #+#             */
-/*   Updated: 2025/02/10 17:11:52 by gvalente         ###   ########.fr       */
+/*   Updated: 2025/02/10 18:47:49 by gvalente         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,14 +17,7 @@ static char	*get_cd_path(t_data *d, char *arg)
 	char	*path;
 
 	path = NULL;
-	if (!arg || !ft_strncmp(arg, "~", 2))
-	{
-		if (d->home_wd == NULL)
-			custom_exit(d, "Home wd not found", NULL, EXIT_FAILURE);
-		else
-			path = ms_strdup(d, d->home_wd);
-	}
-	else if (!ft_strncmp(arg, "-", 2))
+	if (!ft_strncmp(arg, "-", 2))
 	{
 		if (!d->prev_cwd)
 			return (printf("msh cd: OLDPWD not set\n"), NULL);
@@ -44,13 +37,13 @@ int	cd(t_data *d, char *arg, char **flags, int status)
 
 	(void)status;
 	if (flags && flags[0])
-		return (printf("cd: too many arguments\n"), 0);
+		return (printf("msh: cd: too many arguments\n"), 0);
 	path = get_cd_path(d, arg);
 	if (!path)
 		return (FCT_FAIL);
 	if (chdir(path) == -1)
 	{
-		printf("cd: no such file or directory: %s\n", path);
+		printf("msh: cd: %s: No such file or directory\n", path);
 		free(path);
 		return (FCT_FAIL);
 	}
