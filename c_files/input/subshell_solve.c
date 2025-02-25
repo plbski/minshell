ad1821af8b1ccec2bf2b05374a485d8314d2f61e
Giulio Valente <valente95giulio@gmail.com>
2025-02-25 22:21:36 +0100

    corrected sigterm error

--- c_files/input/subshell_solve.c
+++ c_files/input/subshell_solve.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   subshell_solve.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gvalente <gvalente@student.42.fr>          +#+  +:+       +#+        */
+/*   By: giuliovalente <giuliovalente@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/18 01:39:10 by giuliovalen       #+#    #+#             */
-/*   Updated: 2025/02/25 18:51:50 by gvalente         ###   ########.fr       */
+/*   Updated: 2025/02/25 22:01:41 by giuliovalen      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,6 +38,8 @@ static int	solve_subsh_in(t_data *d, t_token *start, t_token *rd)
 		}
 		fd = open(rd->next->name, O_RDONLY);
 	}
+	else
+		return (FCT_FAIL);
 	if (fd == -1)
 		custom_exit(d, "fd read file in subsh_in", NULL, EXIT_FAILURE);
 	pid = fork();
@@ -73,7 +75,7 @@ static char	*get_subsh_output(t_data *d, t_token *start, int rd_fd)
 	waitpid(pid, &status, 0);
 	close(pipefd[1]);
 	setstr(d, &ret, get_fd_content(d, pipefd[0]));
-	return (ret);
+	return (close(pipefd[0]), ret);
 }
 
 static t_token	*get_subsh_out(t_data *d, t_token *start, t_token *redir)
--- c_files/tools/signal.c
+++ c_files/tools/signal.c
@@ -6,7 +6,7 @@
 /*   By: giuliovalente <giuliovalente@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/16 16:14:36 by gvalente          #+#    #+#             */
-/*   Updated: 2025/02/17 23:20:30 by giuliovalen      ###   ########.fr       */
+/*   Updated: 2025/02/25 22:18:20 by giuliovalen      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,25 +20,10 @@ static void	sigint_handler(int sig __attribute__((unused)))
 	rl_redisplay();
 }
 
-/**
- * @note setup CTRL-\. It does nothing but the prompt as to be redisplayed
- * no error possible
- * @attention 
- */
 static void	sigquit_handler(int sig __attribute__((unused)))
 {
 	rl_on_new_line();
 	rl_redisplay();
-}
-
-/**
- * @note Ctrl-d handler - must free stuff on quit
- * @attention Carefull not to create zombie processes on quit!
- * @todo Call function exit() and redirect to that function
- */
-static void	sigterm_handler(int sig __attribute__((unused)))
-{
-	return ;
 }
 
 static void	sigint_handler_heredoc(int sig)
@@ -68,7 +53,7 @@ void	setup_signal(int is_waiting, int is_heredoc)
 	sa_quit.sa_flags = 0;
 	sigemptyset(&sa_quit.sa_mask);
 	sigaction(SIGQUIT, &sa_quit, NULL);
-	sa_term.sa_handler = sigterm_handler;
+	sa_term.sa_handler = SIG_IGN;
 	sa_term.sa_flags = 0;
 	sigemptyset(&sa_term.sa_mask);
 	sigaction(SIGTERM, &sa_term, NULL);
